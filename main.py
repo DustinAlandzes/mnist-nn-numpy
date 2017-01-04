ff4a582b17f1ae854bafc5669763e8769f38c5b2
f00 <f00@f00s-MacBook-Pro.local>
2017-01-03 20:10:08 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import network
+import mnist_loader
+
+# get training data
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+# make network
+# sizes: input(784), hidden(30), output(10)
+net = network.Network([784, 30, 10])
+
+# train network using stochasitc gradient descent
+# over 30 epochs, with a mini-batch size of 10, and a learning rate of n=3.0
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+# exercise:
+# network with just two layers, input and output (w/ 784 and 10 neurons respectively)
+# train using stochastic gradient descent
+net = network.Network([784, 0, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
---
+++ sigmoid.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
